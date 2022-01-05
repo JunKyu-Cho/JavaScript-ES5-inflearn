26612b0e3be9c7addb470452b1064a4f8724b1b5
JK <jk0619@nate.com>
2022-01-05 18:20:42 +0900

    2022. 01. 05 Update

--- js/_.js
+++ js/_.js
@@ -21,10 +21,22 @@ var _get = _curryR(function(obj, key) {
     return obj == null ? undefined : obj[key];
 });
 
+function _isObject(obj) {
+    return typeof obj == 'object' && !!obj;
+}
+function _keys(obj) {
+    return _isObject(obj) ? Object.keys(obj) : [];
+}
+
 var _length = _get('length');
 function _each(list, iter) {
-    for (var II = 0, len = _length(list); II < len; II++) {
-        iter(list[II]);
+    var keys = _keys(list);
+    // for (var II = 0, len = _length(list); II < len; II++) {    
+    //     iter(list[II]);
+    // }
+
+    for (var II = 0, len = keys.length; II < len; II++) {
+        iter(list[keys[II]]);
     }
 
     return list;
@@ -40,6 +52,20 @@ function _curryR(fn) {
     return function (a, b) {
         return arguments.length == 2 ? fn(a, b): function(b) { return fn(b, a); }       
     }
+}
+
+function _pipe() {
+    var fns = arguments;
+    return function(arg) {
+        return _reduce(fns, function(arg, fn) {
+            return fn(arg);                            
+        }, arg);
+    }
+}
+
+function _go(arg) {
+    var fns = _rest(arguments);
+    return _pipe.apply(null, fns)(arg);
 }
 
 var slice = Array.prototype.slice;
@@ -64,16 +90,6 @@ var _map = _curryR(_map),
  _filter = _curryR(_filter);
 
 
- function _pipe() {
-    var fns = arguments;
-    return function(arg) {
-        return _reduce(fns, function(arg, fn) {
-            return fn(arg);                            
-        }, arg);
-    }
-}
+ 
 
-function _go(arg) {
-    var fns = _rest(arguments);
-    return _pipe.apply(null, fns)(arg);
-}
+
